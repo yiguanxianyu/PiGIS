3d60b5533afd9d98720e8647913a0e9538a25dfb
yiguanxianyu <xianyu@pku.edu.cn>
2022-04-01 17:09:13 +0800

    Small change

--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -16,35 +16,57 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QLCDNumber, QMainWindow,
-    QMenu, QMenuBar, QSizePolicy, QStatusBar,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QGraphicsView, QMainWindow, QMenu,
+    QMenuBar, QSizePolicy, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(1055, 764)
+        MainWindow.setLocale(QLocale(QLocale.English, QLocale.UnitedStates))
+        MainWindow.setAnimated(True)
+        self.actionNew = QAction(MainWindow)
+        self.actionNew.setObjectName(u"actionNew")
+        self.actionOpen = QAction(MainWindow)
+        self.actionOpen.setObjectName(u"actionOpen")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.checkBox = QCheckBox(self.centralwidget)
-        self.checkBox.setObjectName(u"checkBox")
-        self.checkBox.setGeometry(QRect(20, 10, 111, 20))
-        self.lcdNumber = QLCDNumber(self.centralwidget)
-        self.lcdNumber.setObjectName(u"lcdNumber")
-        self.lcdNumber.setGeometry(QRect(310, 360, 64, 23))
+        self.graphicsView = QGraphicsView(self.centralwidget)
+        self.graphicsView.setObjectName(u"graphicsView")
+        self.graphicsView.setGeometry(QRect(200, 20, 841, 691))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 22))
-        self.menutest1 = QMenu(self.menubar)
-        self.menutest1.setObjectName(u"menutest1")
+        self.menubar.setGeometry(QRect(0, 0, 1055, 22))
+        self.menuLayer = QMenu(self.menubar)
+        self.menuLayer.setObjectName(u"menuLayer")
+        self.menuSettings = QMenu(self.menubar)
+        self.menuSettings.setObjectName(u"menuSettings")
+        self.menuVector = QMenu(self.menubar)
+        self.menuVector.setObjectName(u"menuVector")
+        self.menuRaster = QMenu(self.menubar)
+        self.menuRaster.setObjectName(u"menuRaster")
+        self.menuHelp = QMenu(self.menubar)
+        self.menuHelp.setObjectName(u"menuHelp")
+        self.menuFile = QMenu(self.menubar)
+        self.menuFile.setObjectName(u"menuFile")
+        self.menuProject = QMenu(self.menubar)
+        self.menuProject.setObjectName(u"menuProject")
+        self.menuView = QMenu(self.menubar)
+        self.menuView.setObjectName(u"menuView")
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
-        self.menubar.addAction(self.menutest1.menuAction())
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuProject.menuAction())
+        self.menubar.addAction(self.menuView.menuAction())
+        self.menubar.addAction(self.menuLayer.menuAction())
+        self.menubar.addAction(self.menuSettings.menuAction())
+        self.menubar.addAction(self.menuVector.menuAction())
+        self.menubar.addAction(self.menuRaster.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+        self.menuFile.addAction(self.actionNew)
+        self.menuFile.addAction(self.actionOpen)
 
         self.retranslateUi(MainWindow)
 
@@ -52,8 +74,16 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.checkBox.setText(QCoreApplication.translate("MainWindow", u"\u4f4f\u5728\u5c71\u91cc\u9488\u4e0d\u6233", None))
-        self.menutest1.setTitle(QCoreApplication.translate("MainWindow", u"\u9488\u4e0d\u6233", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"PiGIS", None))
+        self.actionNew.setText(QCoreApplication.translate("MainWindow", u"New", None))
+        self.actionOpen.setText(QCoreApplication.translate("MainWindow", u"Open", None))
+        self.menuLayer.setTitle(QCoreApplication.translate("MainWindow", u"Layer", None))
+        self.menuSettings.setTitle(QCoreApplication.translate("MainWindow", u"Settings", None))
+        self.menuVector.setTitle(QCoreApplication.translate("MainWindow", u"Vector", None))
+        self.menuRaster.setTitle(QCoreApplication.translate("MainWindow", u"Raster", None))
+        self.menuHelp.setTitle(QCoreApplication.translate("MainWindow", u"Help", None))
+        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"Project", None))
+        self.menuProject.setTitle(QCoreApplication.translate("MainWindow", u"Edit", None))
+        self.menuView.setTitle(QCoreApplication.translate("MainWindow", u"View", None))
     # retranslateUi
 
