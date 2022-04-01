b0dcec59c0011503037976d93e1558f435b86a62
yiguanxianyu <xianyu@pku.edu.cn>
2022-04-01 09:46:05 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# PiGIS
-Project for course
+# πGIS
+Team project for course *Designing And Applications of GIS*
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # File: main.py
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import QFile
+# from PySide6.QtCore import QFile
 from ui.mainwindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
