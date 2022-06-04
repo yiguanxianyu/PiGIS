2ef622025fdad0197ba37a45dda411d9a8ef5998
yiguanxianyu <xianyu@pku.edu.cn>
2022-06-05 02:40:46 +0800

    add attr table

--- ui/AttributesTable.py
+++ ui/AttributesTable.py
@@ -1,8 +1,58 @@
+import pandas as pd
+from PySide6.QtCore import QAbstractTableModel, Qt
 from PySide6.QtWidgets import QWidget, QAbstractItemView
 
 from ui.FieldTable import FieldTable
 from ui.FilterDialog import FilterDialog
 from ui.raw import Ui_AttributesTable
+
+
+class TableModel(QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+        self.__row_count=data.shape[0]
+        self.__column_count=data.shape[1]
+        self.edited = {}
+        self.deleted = []
+
+    def flags(self, index):
+        return Qt.ItemIsEditable | Qt.ItemIsEnabled | Qt.ItemIsSelectable
+
+    def data(self, index, role: int = ...):
+        match role:
+            case Qt.ItemDataRole.DisplayRole | Qt.ItemDataRole.EditRole:
+                if index in self.edited:
+                    return self.edited[index]
+                # TODO: Delete Item
+                return str(self._data.iloc[index.row(), index.column()])
+            case _:
+                pass
+
+    def setData(self, index, value, role: int = ...):
+        self.edited[index] = value
+        # TODO 检查数据类型是否符合
+        return True
+
+    def save_edited_data(self):
+        for key, value in self.edited.items():
+            self._data.iloc[key.row(), key.column()] = value
+        self.edited.clear()
+
+    def rowCount(self, index=...):
+        return self.__row_count
+
+    def columnCount(self, index=...):
+        return self.__column_count
+
+    def headerData(self, section, orientation, role=...):
+        # section is the index of the column/row.
+        if role == Qt.ItemDataRole.DisplayRole:
+            if orientation == Qt.Orientation.Horizontal:
+                return str(self._data.columns[section])
+
+            if orientation == Qt.Orientation.Vertical:
+                return str(self._data.index[section])
 
 
 class AttributesTable(QWidget):
@@ -13,8 +63,20 @@ class AttributesTable(QWidget):
         self.ui = Ui_AttributesTable()
         self.ui.setupUi(self)
 
+        data = pd.DataFrame([
+            [1, 9, 2],
+            [1, 0, -1],
+            [3, 5, 2],
+            [3, 3, 2],
+            [5, 8, 9],
+        ], columns=['A', 'B', 'C'])
+
+        # table = TableModel(layer.get_attr_table())
+        self.table = TableModel(data)
+        self.ui.tableView.setModel(self.table)
+
     def save(self):
-        ...
+        self.table.save_edited_data()
 
     def edit_field(self):
         datatype = None
@@ -25,6 +87,9 @@ class AttributesTable(QWidget):
         ...
 
     def delete_item(self):
+        """
+        这个好像有点难做啊
+        """
         ...
 
     def filter_data(self):
@@ -34,7 +99,7 @@ class AttributesTable(QWidget):
     def toggle_editing_changed(self, flag):
         if flag:
             self.editState = True
-            self.ui.dataTable.setEditTriggers(QAbstractItemView.DoubleClicked)
+            self.ui.tableView.setEditTriggers(QAbstractItemView.DoubleClicked)
         else:
             self.editState = False
-            self.ui.dataTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
+            self.ui.tableView.setEditTriggers(QAbstractItemView.NoEditTriggers)
--- ui/raw/_attributesTable.py
+++ ui/raw/_attributesTable.py
@@ -16,51 +16,18 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractItemView, QApplication, QCheckBox, QGridLayout,
-    QHBoxLayout, QHeaderView, QSizePolicy, QTableWidget,
-    QTableWidgetItem, QToolButton, QWidget)
+    QHBoxLayout, QHeaderView, QSizePolicy, QTableView,
+    QToolButton, QWidget)
 
 class Ui_AttributesTable(object):
     def setupUi(self, AttributesTable):
         if not AttributesTable.objectName():
             AttributesTable.setObjectName(u"AttributesTable")
-        AttributesTable.resize(739, 401)
+        AttributesTable.setWindowModality(Qt.ApplicationModal)
+        AttributesTable.resize(747, 401)
         AttributesTable.setContextMenuPolicy(Qt.NoContextMenu)
         self.gridLayout = QGridLayout(AttributesTable)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.dataTable = QTableWidget(AttributesTable)
-        if (self.dataTable.columnCount() < 5):
-            self.dataTable.setColumnCount(5)
-        __qtablewidgetitem = QTableWidgetItem()
-        self.dataTable.setHorizontalHeaderItem(0, __qtablewidgetitem)
-        __qtablewidgetitem1 = QTableWidgetItem()
-        self.dataTable.setHorizontalHeaderItem(1, __qtablewidgetitem1)
-        __qtablewidgetitem2 = QTableWidgetItem()
-        self.dataTable.setHorizontalHeaderItem(2, __qtablewidgetitem2)
-        __qtablewidgetitem3 = QTableWidgetItem()
-        self.dataTable.setHorizontalHeaderItem(3, __qtablewidgetitem3)
-        __qtablewidgetitem4 = QTableWidgetItem()
-        self.dataTable.setHorizontalHeaderItem(4, __qtablewidgetitem4)
-        if (self.dataTable.rowCount() < 5):
-            self.dataTable.setRowCount(5)
-        __qtablewidgetitem5 = QTableWidgetItem()
-        self.dataTable.setVerticalHeaderItem(0, __qtablewidgetitem5)
-        __qtablewidgetitem6 = QTableWidgetItem()
-        self.dataTable.setVerticalHeaderItem(1, __qtablewidgetitem6)
-        __qtablewidgetitem7 = QTableWidgetItem()
-        self.dataTable.setVerticalHeaderItem(2, __qtablewidgetitem7)
-        __qtablewidgetitem8 = QTableWidgetItem()
-        self.dataTable.setVerticalHeaderItem(3, __qtablewidgetitem8)
-        __qtablewidgetitem9 = QTableWidgetItem()
-        self.dataTable.setVerticalHeaderItem(4, __qtablewidgetitem9)
-        __qtablewidgetitem10 = QTableWidgetItem()
-        self.dataTable.setItem(1, 1, __qtablewidgetitem10)
-        __qtablewidgetitem11 = QTableWidgetItem()
-        self.dataTable.setItem(2, 2, __qtablewidgetitem11)
-        self.dataTable.setObjectName(u"dataTable")
-        self.dataTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
-
-        self.gridLayout.addWidget(self.dataTable, 1, 0, 1, 1)
-
         self.widget = QWidget(AttributesTable)
         self.widget.setObjectName(u"widget")
         self.widget.setMinimumSize(QSize(0, 25))
@@ -102,6 +69,14 @@ class Ui_AttributesTable(object):
 
         self.gridLayout.addWidget(self.widget, 0, 0, 1, 1)
 
+        self.tableView = QTableView(AttributesTable)
+        self.tableView.setObjectName(u"tableView")
+        self.tableView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.tableView.setGridStyle(Qt.SolidLine)
+        self.tableView.setSortingEnabled(True)
+
+        self.gridLayout.addWidget(self.tableView, 1, 0, 1, 1)
+
 
         self.retranslateUi(AttributesTable)
         self.editFieldButton.clicked.connect(AttributesTable.edit_field)
@@ -116,35 +91,6 @@ class Ui_AttributesTable(object):
 
     def retranslateUi(self, AttributesTable):
         AttributesTable.setWindowTitle(QCoreApplication.translate("AttributesTable", u"Attributes Table", None))
-        ___qtablewidgetitem = self.dataTable.horizontalHeaderItem(0)
-        ___qtablewidgetitem.setText(QCoreApplication.translate("AttributesTable", u"\u52171", None));
-        ___qtablewidgetitem1 = self.dataTable.horizontalHeaderItem(1)
-        ___qtablewidgetitem1.setText(QCoreApplication.translate("AttributesTable", u"\u65b0\u5efa\u5217", None));
-        ___qtablewidgetitem2 = self.dataTable.horizontalHeaderItem(2)
-        ___qtablewidgetitem2.setText(QCoreApplication.translate("AttributesTable", u"\u52172", None));
-        ___qtablewidgetitem3 = self.dataTable.horizontalHeaderItem(3)
-        ___qtablewidgetitem3.setText(QCoreApplication.translate("AttributesTable", u"3", None));
-        ___qtablewidgetitem4 = self.dataTable.horizontalHeaderItem(4)
-        ___qtablewidgetitem4.setText(QCoreApplication.translate("AttributesTable", u"test5", None));
-        ___qtablewidgetitem5 = self.dataTable.verticalHeaderItem(0)
-        ___qtablewidgetitem5.setText(QCoreApplication.translate("AttributesTable", u"1", None));
-        ___qtablewidgetitem6 = self.dataTable.verticalHeaderItem(1)
-        ___qtablewidgetitem6.setText(QCoreApplication.translate("AttributesTable", u"2", None));
-        ___qtablewidgetitem7 = self.dataTable.verticalHeaderItem(2)
-        ___qtablewidgetitem7.setText(QCoreApplication.translate("AttributesTable", u"3", None));
-        ___qtablewidgetitem8 = self.dataTable.verticalHeaderItem(3)
-        ___qtablewidgetitem8.setText(QCoreApplication.translate("AttributesTable", u"4", None));
-        ___qtablewidgetitem9 = self.dataTable.verticalHeaderItem(4)
-        ___qtablewidgetitem9.setText(QCoreApplication.translate("AttributesTable", u"5", None));
-
-        __sortingEnabled = self.dataTable.isSortingEnabled()
-        self.dataTable.setSortingEnabled(False)
-        ___qtablewidgetitem10 = self.dataTable.item(1, 1)
-        ___qtablewidgetitem10.setText(QCoreApplication.translate("AttributesTable", u"test1", None));
-        ___qtablewidgetitem11 = self.dataTable.item(2, 2)
-        ___qtablewidgetitem11.setText(QCoreApplication.translate("AttributesTable", u"test2", None));
-        self.dataTable.setSortingEnabled(__sortingEnabled)
-
         self.editFieldButton.setText(QCoreApplication.translate("AttributesTable", u"Edit Field", None))
         self.addItemButton.setText(QCoreApplication.translate("AttributesTable", u"Add Item", None))
         self.deleteItemButton.setText(QCoreApplication.translate("AttributesTable", u"Delete Item", None))
